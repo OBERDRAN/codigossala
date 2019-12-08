20f1febe591d0db172c34bb73aa97d194a17a037
OBERDRAN <oberdran123@gmail.com>
2019-12-08 16:44:52 -0300

    Verificação de dias, meses e anos

---
+++ CodigosSala/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigosSala
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            int dia, mes, ano;
+
+            Console.Write("Coloque o Dia: ");
+            dia = int.Parse(Console.ReadLine());
+            
+           if (dia <= 31){
+
+                Console.Write("Lendo Mes: ");
+                mes = int.Parse(Console.ReadLine());
+
+                if (mes <= 12){
+                    Console.WriteLine("Anos entre 1900 e 2100\n");
+                    Console.Write("Lendo Ano: ");
+
+                    ano = int.Parse(Console.ReadLine());
+
+                    if (ano >= 1900 && ano <= 2100)
+                     {
+                         Console.WriteLine("Tudo Correto");
+                         Console.WriteLine("\n");
+
+                         Console.ReadKey();
+            
+                        //tem que chamar main() aqui
+
+
+                     }else{
+
+                        Console.WriteLine("Ano Invalido");
+                        Console.WriteLine("\n");
+                        Console.ReadKey();
+                        //tem que chamar main() aqui
+                     }
+
+                }
+                else{
+                    Console.WriteLine("Mes Invalido, digite novamente! ");
+                    Console.WriteLine("\n");
+                    Console.ReadKey();
+
+                    
+                    //tem que chamar main() aqui
+                }
+
+            }
+            else{
+                Console.WriteLine("Dia Invalido, digite novamente!");
+                Console.WriteLine("\n");
+                Console.ReadKey();
+
+                //tem que chamar main() aqui
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # codigossala
+
+#Codigos feitos na sala de aula, durante o período de 2019.2
